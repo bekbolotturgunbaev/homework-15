c8ce26111bf8ff2b1ec98e0f7963179da5b73210
bekbolotturgunbaev <bekaturgunbaev007@gmail.com>
2022-10-15 18:39:54 +0300

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,13 @@
+public class Circle {
+    private static final double Pi=3.14;
+
+    public static void area(int radius) {
+        System.out.println(Pi*(radius*radius));
+    }
+
+    public static void  circumference(int radius) {
+        System.out.println(Pi*2*radius);
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Circle.area(11);
+        Circle.circumference(11);
+
+
+
+    }
+}
